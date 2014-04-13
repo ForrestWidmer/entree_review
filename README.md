bdc257c19b1a847c5cc3f2c5a10ff385e7cd564d
ForrestWidmer <forrest.widmer@gmail.com>
2014-04-13 14:18:44 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+#Entree Review!
+
+Find and rate the best entree and meal near you!
+
+Feel like a Bacon Burger? Find the best one near you with reviews and pictures submitted by other users!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EntreeReview::Application.config.secret_token = '1b133cefb8074b9e7c515a5395b5b70fcd57e287e4572936cc25ca07dcbae8e50a75c96da878e07a2adb38afd021adfc4aff2c409007c1e9df09ad2a4d892cc5'
